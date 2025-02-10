1f14aebd3e34786f315b5defe24fa852fe009961
Suraj kumar jha <surajkumarjha771@gmail.com>
2025-02-10 19:38:31 +0530

    app is completed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: '0.0.0.0'
+  },
   plugins: [
     react(),
     tailwindcss()
